329395720a48e33369362de5fb44d9fecd31f660
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-12-12 08:58:21 -0400

    Console Output Challenge

---
+++ ConsoleOutputChallenge/ConsoleOutputChallenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOutputChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 33;
+            
+            if (age > 17)
+            {
+                Console.WriteLine("You are an adult.");
+            }
+            else if (age <= 17)
+            {
+                Console.WriteLine("You are a child.");
+            }
+            else
+            {
+                Console.WriteLine("I don't have your age.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
