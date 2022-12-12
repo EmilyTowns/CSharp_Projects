068279864d335a4c726ba44910ef4de12082a630
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-12-12 08:13:28 -0400

    Conditional Statments Challenge

---
+++ ConditionalStatementsChallenge/ConditionalStatementsChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatementsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string car = "Challenger";
+
+            if(car == "Corvette")
+            {
+                Console.WriteLine("You own a Corvette.");
+            }
+            else if (car == "Challenger")
+            {
+                Console.WriteLine("You own a Challenger.");
+            }
+            else if (car == "Mustang")
+            {
+                Console.WriteLine("You own a Mustang.");
+            }
+            else
+            {
+                Console.WriteLine("You do not own a cool car.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
