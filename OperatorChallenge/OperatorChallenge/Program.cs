25dd84ec41ed751cb917e7ea09939cbf228d6969
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-12-12 08:41:43 -0400

    Operator Challenge

---
+++ OperatorChallenge/OperatorChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (16 != 8)
+            {
+                Console.WriteLine("16 is not equal to 8.");
+            }
+            else
+            {
+                Console.WriteLine("How strange. 16 is somehow equal to 8.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
