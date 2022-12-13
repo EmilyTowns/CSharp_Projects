47bce94c58783daa0f49254029296ff8195745a2
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-12-13 08:57:35 -0400

    Ternary Operator Challenge

---
+++ TernaryOperatorChallenge/TernaryOperatorChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 50;
+            int num2 = 25;
+
+            string result = num1 == num2 ? "50 is equal to 25" : "50 is not equal to 25";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
