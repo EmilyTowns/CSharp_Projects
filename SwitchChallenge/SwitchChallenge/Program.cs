22730bbf7910979a9b7af8a5cea4cb28bb2cdd2f
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2022-12-16 08:54:48 -0400

    Switch Challenge

---
+++ SwitchChallenge/SwitchChallenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meal = 3;
+            switch (meal)
+            {
+                case 1:
+                    Console.WriteLine("Chicken Parm");
+                    break;
+                case 2:
+                    Console.WriteLine("Pasta Alfredo");
+                    break;
+                case 3:
+                    Console.WriteLine("Mac and Cheese");
+                    break;
+                case 4:
+                    Console.WriteLine("Burger and Fries");
+                    break;
+                case 5:
+                    Console.WriteLine("Sushi");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
