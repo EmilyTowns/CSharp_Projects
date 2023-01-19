44096d0e8730e5cff1b472a8b06e531bd633480b
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-01-19 09:49:11 -0400

    Update twenty one and calling methods assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Age.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class Age
+    {
+        public int age;
+        
+        public void Age5(int input)
+        {
+            Age age5 = new Age();
+            age5.age = input;
+            Console.WriteLine("In 5 years, you will be " + age5.age + ".");
+        }
+
+        public void Age10(int input)
+        {
+            Age age10 = new Age();
+            age10.age = input;
+            Console.WriteLine("In 10 years, you will be " + age10.age + ".");
+        }
+
+        public void Age15(int input)
+        {
+            Age age15 = new Age();
+            age15.age = input;
+            Console.WriteLine("In 15 years, you will be " + age15.age + ".");
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's see what age you will be in 5, 10, and 15 years.");
+            Console.WriteLine("Please enter your current age:"); }
+
+            public int input = Convert.ToInt32(Console.ReadLine());
+
+            age5.age(input);
+
+            Console.ReadLine();
+        
+    }
+}
