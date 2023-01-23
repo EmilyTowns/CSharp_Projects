f9d0a57ca536093e80445ce6e8a2d35d1e4623a5
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-01-23 09:24:09 -0400

    Main Method Assingment Submission

---
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Math   //Creates a new public class
+    {
+        public static int math(int number1)   //Creates a new method where an integer can be passed in 
+        {
+            number1 = number1 * 10;   //Takes the input integer and multiples by 10
+            return number1;   //Returns the result
+        }
+
+        public static int math(decimal number2)   //Overloads the method by adding the option to pass in a decimal number
+        {
+            number2 = number2 / 2;   //Divides the number by 2
+            int number3 = Convert.ToInt32(number2);   //Takes the decimal result and converts it to an integer
+            return number3;   //Returns the integer result
+        }
+
+        public static int math(string words)   //Overloads the method again by adding the option to pass in a string value
+        {
+            int number5 = Convert.ToInt32(words);   //Converts the string value to an integer
+            number5 = number5 + 100;   //Add 100 to the converted intger
+            return number5;   //Returns the result
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 9;   //Assigns 9 to the integer number
+            int multiple = Math.math(number);   //Creates a new integer and calls the function, passing it the integer number
+            Console.WriteLine("{0} multiplied by 10 equals {1}.", number, multiple);   //Writes the results to the console
+
+            decimal number4 = 904556;   //Assigns 904556 to a decimal 
+            int convert = Math.math(number4);   //Creates a new integer and calls the function, passing it the decimal number4
+            Console.WriteLine("{0} divided by 2 equals {1}.", number4, convert);   //Writes the results to the console
+
+            string words = "55";   //Assigns "55" to a string
+            int fromString = Math.math(words);   //Creates a new integer and calls the function, passing it the string value
+            Console.WriteLine("{0} plus 100 equals {1}.", words, fromString);   //Writes the results to the console
+            Console.ReadLine();   //Keeps the console open
+        }
+    }
+}
