27d6812cd956a79de70c1d61a3a0e13d73605c9e
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-01-23 08:12:04 -0400

    Update Calling Methods Assignment with comments

--- CallingMethodsAssignment/CallingMethodsAssignment/Age.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Age.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace CallingMethodsAssignment
 {
-    public class Ages
+    public class Ages   //Creates public class Ages
     {
-        public static int Age5(int addFive)
+        public static int Age5(int addFive)   //Creates method and creates a new integer addFive, to which currentAge can be assigned when it is called in the Program class
         { 
-            addFive = addFive + 5;
-            return addFive;
+            addFive = addFive + 5;   //Adds five years to the input age
+            return addFive;   //Returns the new value of addFive, which adds 5 years - this will provide the result when method is called
         }
 
-        public static int Age10(int addTen)
+        public static int Age10(int addTen)   //Creates method and creates a new integer addTen, to which currentAge can be assigned when it is called in the Program class
         {
-            addTen = addTen + 10;
-            return addTen;
+            addTen = addTen + 10;   //Adds ten years to the input age
+            return addTen;   //Returns the new value of addTen, which adds 10 years - this will provide the result when method is called
         }
 
-        public static int Age15(int addFifteen)
+        public static int Age15(int addFifteen)   //Creates method and creates a new integer addFifteen, to which currentAge can be assigned when it is called in the Program class
         {
-            addFifteen = addFifteen + 15;
-            return addFifteen;
+            addFifteen = addFifteen + 15;   //Adds fifteen years to the input age
+            return addFifteen;   //Returns the new value of addFifteen, which adds 15 years - this will provide the result when method is called
         }
     }   
 }
--- CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -10,20 +10,20 @@ namespace CallingMethodsAssignment
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Let's see how old you will be in 5, 10, and 15 years.");
-            Console.WriteLine("Please enter your current age:");
-            int currentAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Let's see how old you will be in 5, 10, and 15 years.");   //Writes to console
+            Console.WriteLine("Please enter your current age:");   //Writes to console asking for user input
+            int currentAge = Convert.ToInt32(Console.ReadLine());   //Stores user input, converting to int variable
 
-            int five = Ages.Age5(currentAge);
-            Console.WriteLine("In 5 years, you will be " + five + ".");
+            int five = Ages.Age5(currentAge);   //Accesses Age class and Age5 method, using currentAge as input for the method
+            Console.WriteLine("In 5 years, you will be " + five + ".");   //Writes result to console
 
-            int ten = Ages.Age10(currentAge);
-            Console.WriteLine("In 10 years, you will be " + ten + ".");
+            int ten = Ages.Age10(currentAge);   //Accesses Age class and Age10 method, using currentAge as input for the method
+            Console.WriteLine("In 10 years, you will be " + ten + ".");   //Writes result to console
 
-            int fifteen = Ages.Age15(currentAge);
-            Console.WriteLine("In 15 years, you will be " + fifteen + ".");
+            int fifteen = Ages.Age15(currentAge);   //Accesses Age class and Age10 method, using currentAge as input for the method
+            Console.WriteLine("In 15 years, you will be " + fifteen + ".");   //Writes result to console
 
-            Console.ReadLine();
+            Console.ReadLine();   //Keeps the console open
         }       
         
         
