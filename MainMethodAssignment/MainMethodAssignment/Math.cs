1d34ee974241bca6d9f95b65acbfc24b4b2201d4
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-01-24 09:58:07 -0400

    Corrected main method assignment with instantiation

--- MainMethodAssignment/MainMethodAssignment/Math.cs
+++ MainMethodAssignment/MainMethodAssignment/Math.cs
@@ -8,24 +8,49 @@ namespace MainMethodAssignment
 {
     public class Math   //Creates a new public class
     {
-        public static int math(int number1)   //Creates a new method where an integer can be passed in 
+        int number1;
+        decimal number2;
+        string words;
+
+        public int math(int number1)
         {
-            number1 = number1 * 10;   //Takes the input integer and multiples by 10
-            return number1;   //Returns the result
+            number1 = number1 * 10;
+            return number1;
         }
 
-        public static int math(decimal number2)   //Overloads the method by adding the option to pass in a decimal number
+        public int math(decimal number2)
         {
-            number2 = number2 / 2;   //Divides the number by 2
-            int number3 = Convert.ToInt32(number2);   //Takes the decimal result and converts it to an integer
-            return number3;   //Returns the integer result
+            number2 = number2 / 2;
+            int number3 = Convert.ToInt32(number2);
+            return number3;
         }
 
-        public static int math(string words)   //Overloads the method again by adding the option to pass in a string value
+        public int math(string words)
         {
-            int number5 = Convert.ToInt32(words);   //Converts the string value to an integer
-            number5 = number5 + 100;   //Add 100 to the converted intger
-            return number5;   //Returns the result
+            int number5 = Convert.ToInt32(words);
+            number5 = number5 + 100;
+            return number5;
         }
+
+
+        //public static int math(int number1)   //Creates a new method where an integer can be passed in 
+        //{
+        //    number1 = number1 * 10;   //Takes the input integer and multiples by 10
+        //       //Returns the result
+        //}
+
+        //public static int math(decimal number2)   //Overloads the method by adding the option to pass in a decimal number
+        //{
+        //    number2 = number2 / 2;   //Divides the number by 2
+        //    int number3 = Convert.ToInt32(number2);   //Takes the decimal result and converts it to an integer
+        //    return number3;   //Returns the integer result
+        //}
+
+        //public static int math(string words)   //Overloads the method again by adding the option to pass in a string value
+        //{
+        //    int number5 = Convert.ToInt32(words);   //Converts the string value to an integer
+        //    number5 = number5 + 100;   //Add 100 to the converted intger
+        //    return number5;   //Returns the result
+        //}
     }
 }
--- MainMethodAssignment/MainMethodAssignment/Program.cs
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace MainMethodAssignment
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            int number = 9;   //Assigns 9 to the integer number
-            int multiple = Math.math(number);   //Creates a new integer and calls the function, passing it the integer number
-            Console.WriteLine("{0} multiplied by 10 equals {1}.", number, multiple);   //Writes the results to the console
+            int number1 = 9;
+            decimal number2 = 904556;
+            string words = "55";
 
-            decimal number4 = 904556;   //Assigns 904556 to a decimal 
-            int convert = Math.math(number4);   //Creates a new integer and calls the function, passing it the decimal number4
-            Console.WriteLine("{0} divided by 2 equals {1}.", number4, convert);   //Writes the results to the console
+            var math = new Math();
+            var a = math.math(number1);
+            Console.WriteLine("{0} multiplied by 10 equals {1}.", number1, a);
 
-            string words = "55";   //Assigns "55" to a string
-            int fromString = Math.math(words);   //Creates a new integer and calls the function, passing it the string value
-            Console.WriteLine("{0} plus 100 equals {1}.", words, fromString);   //Writes the results to the console
+            var math1 = new Math();
+            var b = math1.math(number2);
+            Console.WriteLine("{0} divided by 2 equals {1}.", number2, b);
+
+            var math2 = new Math();
+            var c = math2.math(words);
+            Console.WriteLine("{0} plus 100 equals {1}.", words, c);
+
             Console.ReadLine();   //Keeps the console open
         }
     }
