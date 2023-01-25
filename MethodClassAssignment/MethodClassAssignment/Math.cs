c13a0f8774e94c58cfb76a3faf63ca378ba010ba
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-01-25 12:28:17 -0400

    Method Class Assignment completed

---
+++ MethodClassAssignment/MethodClassAssignment/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Math
+    {
+        public static void math(int num1, int num2)
+        {
+            int num3 = num1 * num2;
+            Console.WriteLine("{0} times {1} equals {2}.", num1, num2, num3);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Math.math(20, 9);
+
+            Math.math(num1: 40, num2: 2);
+
+            Console.ReadLine();
+        }
+    }
+}
