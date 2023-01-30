2d6bb0954c3073475e2617e9a6b9074446935fae
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-01-30 09:41:23 -0400

    Polymorphism Assignment Complete

---
+++ PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    public class Employee : Person, IQuittable   //Creates class, inheriting Person class and IQuittable interface
+    {
+        public override void SayName()   //Uses method from Person and adds a writeline instruction
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void Quit()   //Uses method from IQuittable and adds a writeline instruction
+        {
+            Console.WriteLine("This person has quit.");
+        }    
+    }
+}
---
+++ PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee()   //Instantiates class employee and assigns values to firstName and lastName
+            {
+                firstName = "George",
+                lastName = "Sanderson"
+            };
+
+            employee.SayName();   //Calls SayName method, which will write name to console
+
+            IQuittable quittable = new Employee();   //Creates quittable using polymorphism
+
+            quittable.Quit();   //Calls Quit method, which displays that the employee has quit
+
+            Console.ReadLine();   //Keeps the console open
+        }
+    }
+}
