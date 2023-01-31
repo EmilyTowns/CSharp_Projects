0d430d737c88bfc060ea6dfb7765763d1e3a60b4
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-01-31 08:46:01 -0400

    Operators Assignment Complete

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee   //Creates public class Employee
+    {
+        public int Id { get; set; }   //Sets three variables
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator== (Employee Id1, Employee Id2)   //Defines == overload with input of two employees
+        {
+            bool status = false;   //Sets default status boolean to false
+            if (Id1.Id == Id2.Id)   //If the Id of the two employees is the same, changes boolean status to true, otherwise value will remain false
+            {
+                status = true;
+            }
+            return status;   //Returns value of status
+        }
+
+        public static bool operator !=(Employee Id1, Employee Id2)   //Defines != overload with input of two employees (required with comparison operators)
+        {
+            bool status = false;   //Sets default status boolean to false
+            if (Id1.Id != Id2.Id)    //If the Id of the two employees is not the same, changes boolean status to true, otherwise value will remain false
+            {
+                status = true;
+            }
+            return status;   //Returns value of status
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Id1 = new Employee();   //Instatntiates Employee class to demonstrate first employee
+            Employee Id2 = new Employee();   //Instatntiates Employee class to demonstrate second employee
+
+            Id1.Id = 1637;   //Sets values for first employees Id and names
+            Id1.FirstName = "Susan";
+            Id1.LastName = "Sanderson";
+
+            Id2.Id = 9204;   //Sets values for the second employees Id and names
+            Id2.FirstName = "George";
+            Id2.LastName = "Frankton";
+
+            Console.WriteLine("Employee {0} {1} and employee {2} {3} have the same ID:", Id1.FirstName, Id1.LastName, Id2.FirstName, Id2.LastName);   //Writes the names of the two employees to console
+            Console.WriteLine(Id1 == Id2);   //Uses overloaded == operator to compare the two Ids, returning false
+            Console.ReadLine();   //Keeps console open
+        }
+    }
+}
