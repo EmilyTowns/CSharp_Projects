3fd72432eeb401cae6154eed87555a6848264c32
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-02-02 10:01:08 -0400

    Struct Assignment Complete

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    struct Number   //Creates struct
+    {
+        public decimal Amount;   //Creates a decimal property
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1;   //Creates an object of the struct Number 
+            num1.Amount = 9000000;   //Assigns value to the Amount property
+
+            Console.WriteLine(num1.Amount);   //Writes this amount to console
+            Console.ReadLine();   //Keeps console open
+        }
+    }
+}
