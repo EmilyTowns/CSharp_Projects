760119e169c033598817aa82e3a575ebb4629214
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-02-09 09:34:11 -0400

    Constructor Assignment Complete

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string pageName = "User Entry System";   //App title is assigned as a constant because it will not change
+            Console.WriteLine(pageName);   //Writes page name to console
+
+            Console.WriteLine("Enter new user first name:");   //Prompts for entry of new user first name
+            var firstName = Console.ReadLine();   //Assigns user input to a variable, which will be determined to be string input
+            Console.WriteLine("Enter new user last name:");   //Prompts for entry of last name
+            var lastName = Console.ReadLine();   //Assins this input to another variable which will also automatically be assigned as a string
+
+            User user = new User(firstName, lastName);   //Calls User and inputs the first and last name variables
+
+            Console.ReadLine();   //Keeps the console open
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class User   //Makes public user class
+    {
+        public string FirstName { get; set; }   //Sets first and last name properties of class user
+        public string LastName { get; set; }
+
+        public User(string FirstName) : this(FirstName, "Doe")   //Chains constructor in the case of a user not having a last name to input, assigning generic last name as Doe
+        {
+            this.FirstName = FirstName;   
+            Console.WriteLine("New user: {0}", FirstName);   //Would write only first name to console
+        }
+
+        public User(string FirstName, string LastName)   //Used if there is a first and last name input
+        {
+            this.FirstName = FirstName;   //Input first and last name is equal to corresponding properties
+            this.LastName = LastName;
+            Console.WriteLine("New user: {0} {1}", FirstName, LastName);   //Writes first and last name to console
+        }
+    }
+}
