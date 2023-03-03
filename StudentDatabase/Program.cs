b61f2b0d99894f548073be5d4482a67b50cfb8c0
Emily Towns <110913738+EmilyTowns@users.noreply.github.com>
2023-03-03 08:12:34 -0400

    Final Assignment Complete

---
+++ StudentDatabase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext()) 
+            {
+                var stud = new Student()
+                {
+                    StudentName = "Bill",
+                };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
